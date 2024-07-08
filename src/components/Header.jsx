26d8b8cb18036380fbddf6fd64331b3b9ef653cc
temblaze <fakunletemiloluwa09@gmail.com>
2024-07-08 09:57:37 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import reactImg from "../assets/hub.png";
+
+
+
+export default function Header() {
+  
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized house" />
+      <h1>Temblaze's Hub</h1>
+      <p>
+       Alone we can do so little; together we can do so much....
+      </p>
+    </header>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,31 @@
+import chatImg from './assets/group-chat.png';
+import gameImg from './assets/game-controller.png';
+import prizeImg from './assets/championship.png';
+import contactImg from './assets/live-chat.png';
+
+export const CORE_CONCEPTS = [
+  {
+    image: chatImg,
+    title: 'Chat',
+    description:
+      'Get to meet your friends and also new people through real-time interaction.',
+  },
+  {
+    image: gameImg,
+    title: 'Games',
+    description:
+      'Cross platform games are also available to play for your entertainment.',
+  },
+  {
+    image: prizeImg,
+    title: 'Competitions',
+    description:
+      'Participate in competitions among your peers and stand the chance to win amazing prices.',
+  },
+  {
+    image: contactImg,
+    title: 'Talk to us',
+    description:
+      'Get in touch with our 24/7 customer care, we always love to hear from you.',
+  },
+];
